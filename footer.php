930d3b994fbd991d14009bd24d7d7d7a47e001c3
shunmasa <masashilandjob@gmail.com>
2019-08-29 17:02:20 +1200

    Masashi's initial commit css folder and some changes to footer/header

--- footer.php
+++ footer.php
@@ -1,6 +1,54 @@
-<footer>
 
-</footer>
 
-</body>
+ <footer className="bck_b_dark">
+      <div className="container">
+        <div className="logo">
+          Forum <span>X</span>
+        </div>
+        <div class="wrapper">
+          <div class="left">
+            <h2> Content information</h2>
+            <div class="business_nfo">
+              <div class="tag">
+                <!-- <FontAwesomeIcon icon={faCompass} class="icon" /> -->
+                <div class="nfo">
+                  <div>Address</div>
+                  <div>
+                    289 Tuam St, Christchurch Central, Christchurch 8011
+                  </div>
+                </div>
+              </div>
+
+              <div className="tag">
+                <!-- <FontAwesomeIcon icon={faPhone} className="icon" /> -->
+                <div className="nfo">
+                  <div>Phone</div>
+                  <div>09-555 5490</div>
+                </div>
+              </div>
+
+              <div class="tag">
+                <!-- <FontAwesomeIcon icon={faEnvelope} class="icon" /> -->
+                <div class="nfo">
+                  <div>Email</div>
+                  <div>forum_X@gmail.com</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="left">
+            <h2>Description</h2>
+            <div>
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Recusandae dicta veniam eveniet fugit facilis!
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    </body>
 </html>
--- header.php
+++ header.php
@@ -3,12 +3,48 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <title>Forum X</title>
+    <link rel="stylesheet" href="./css/home.css" type="text/css">
+
         <title></title>
     </head>
     <body>
 
+    <div class="head-wrap d-flex">
+          <h1 class="forum flex-grow-1 ">
+                Forum
+                <span>X</span>
+              </h1>
+           
+
+
+           <div class="wrap-button flex-grow-0 ">
+              <button
+                href="/"
+                class="btn btn-outline-primary"
+                
+              >
+                Login
+              </button >
+              <button
+                href="/"
+                class="btn btn-outline-success"
+                
+              >
+                Sign-Up
+             
+             
+              </button>
+            </div>
+        </div>
+
         <header>
-<div>
+<div><!-- initil part from Anton-->
     <?php
         if (isset($_SESSION['userID'])) {
             echo '<form action="scripts/logout.php" method="post">
@@ -25,5 +61,34 @@
         }    
     ?>
     <a href="signup.php">Signup</a>
-</div>
+</div><!-- initial part from Anton-->
+
+
+
+<div class="menu-toggle" id="hamburger"> 
+<!-- <i className="fas fa-bars"></i>            -->
+    </div>
+      <div class="overlay" >
+        <div class="container">   
+          <nav>
+              <ul>
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/forum">forum</a>
+                </li>
+                <li>
+                  <a href="/forum_modal_rule">rule</a>
+                </li>
+            
+              </ul>
+           
+             
+            </nav>
+           </div>
+          </div>
+          </div> 
+
+
         </header>
