3d832c401ffbd7b8c16d55210433dae33782ff1f
AntonILLI <51392226+AntonILLI@users.noreply.github.com>
2021-02-12 10:19:54 +1300

    Update header.php

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
     <link
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
+    <link rel="shortcut icon" href="favicon.ico">
     <script
     src="http://code.jquery.com/jquery-3.3.1.min.js"
     integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
